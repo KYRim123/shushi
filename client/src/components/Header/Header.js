8aaee3016ee6004777e0340f2281200d50f58215
nguyenthanhluong <92586539+KYRim123@users.noreply.github.com>
2022-12-03 20:21:36 +0700

    done

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,79 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom' 
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './headerStyle.scss'
 import images from '../../assets/img'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faClose, faMoon, faBars, faSun} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClose, faMoon, faBars, faSun} from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
     const [showMenu, setShowMenu] = useState(false)
     const [iconTheme, setIconTheme] = useState(faMoon)
+    const [currentTheme, setCurrentTheme] = useState(false)
     const navItem = [
-        {to: '/', name: 'home'},
-        {to: '/', name: 'about us'},
-        {to: '/', name: 'popular'},
-        {to: '/', name: 'recently'},
+        { to: '/', name: 'home' },
+        { to: '/', name: 'about us' },
+        { to: '/', name: 'popular' },
+        { to: '/', name: 'recently' },
     ]
     const handleShowMenu = () => {
         setShowMenu(true)
-    } 
+    }
 
     const handleCloseMenu = () => {
         setShowMenu(false)
-    } 
+    }
     //scroll add box shadow for header
     useEffect(() => {
         const handleScrollHeader = () => {
-          const header = document.getElementById('header')
-          window.scrollY >=50 ? header.classList.add('bg-header'):header.classList.remove('bg-header')
+            const header = document.getElementById('header')
+            window.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
         };
         window.addEventListener('scroll', handleScrollHeader);
         // clean up
         return () => {
-          window.removeEventListener('scroll', handleScrollHeader);
+            window.removeEventListener('scroll', handleScrollHeader);
         };
-      }, []); 
-
-      const handleChangeTheme = () => {
+    }, []);
+    //get theme from local Storage
+    useEffect(() => {
+        const theme = JSON.parse(localStorage.getItem('selected-theme'))
+        setCurrentTheme(theme)
+        theme ? document.body.classList.add('dark--theme'):document.body.classList.remove('dark--theme')
+    },[iconTheme])
+    //handle change theme 
+    const handleChangeTheme = () => {
         document.body.classList.toggle('dark--theme')
-        document.body.classList.contains('dark--theme')?setIconTheme(faSun):setIconTheme(faMoon)
-      }
+        document.body.classList.contains('dark--theme') ? setIconTheme(faSun) : setIconTheme(faMoon)
+        currentTheme ? 
+        localStorage.setItem('selected-theme', JSON.stringify(false)):
+        localStorage.setItem('selected-theme', JSON.stringify(true))
+    }
 
     return (
-    <header className='header' id='header' >
-        <nav className='nav wide'>
-            <Link to='/' className='nav__logo'>
-                <img src={images.logo} alt='logo'/>
-                <h2 className='nav__heading'>sushi</h2>
-            </Link>
-            <div className={`nav__menu ${showMenu ? 'show--menu':''}`}>
-                <ul className='nav__list'>
-                    {
-                        navItem.map((item, index) => 
-                            <li className='nav__item' key={index} onClick={handleCloseMenu}>
-                                <Link to={item.to} className='nav__link'>{item.name}</Link>
-                            </li>
-                        )
-                    }
-                </ul>
-                {/* close button */}
-                <div className="nav__close" id="nav-close" onClick={handleCloseMenu}>
-                    <FontAwesomeIcon icon={faClose}/>
+        <header className='header' id='header' >
+            <nav className='nav wide'>
+                <Link to='/' className='nav__logo'>
+                    <img src={images.logo} alt='logo' />
+                    <h2 className='nav__heading'>sushi</h2>
+                </Link>
+                <div className={`nav__menu ${showMenu ? 'show--menu' : ''}`}>
+                    <ul className='nav__list'>
+                        {
+                            navItem.map((item, index) =>
+                                <li className='nav__item' key={index} onClick={handleCloseMenu}>
+                                    <Link to={item.to} className='nav__link'>{item.name}</Link>
+                                </li>
+                            )
+                        }
+                    </ul>
+                    {/* close button */}
+                    <div className="nav__close" id="nav-close" onClick={handleCloseMenu}>
+                        <FontAwesomeIcon icon={faClose} />
+                    </div>
+                    <img src={images.leafBranch1} alt="navimage" className='nav__img1' />
+                    <img src={images.leafBranch2} alt="navimage" className='nav__img2' />
                 </div>
-                <img src={images.leafBranch1} alt="navimage" className='nav__img1'/>
-                <img src={images.leafBranch2} alt="navimage" className='nav__img2'/>
-            </div>
-            <div className="nav__buttons" >
-                {/* theme change button */}
-                <FontAwesomeIcon icon={iconTheme} onClick={handleChangeTheme} className='change-theme' id='theme-button'/>
-                {/* toggle button */}
-                <div className="nav__tonggle" onClick={handleShowMenu}>
-                    <FontAwesomeIcon icon={faBars}/>
+                <div className="nav__buttons" >
+                    {/* theme change button */}
+                    <FontAwesomeIcon icon={iconTheme} onClick={handleChangeTheme} className='change-theme' id='theme-button' />
+                    {/* toggle button */}
+                    <div className="nav__tonggle" onClick={handleShowMenu}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </header>
-  )
+            </nav>
+        </header>
+    )
 }
 
 export default Header
